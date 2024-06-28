26385641d9d00ccdcd8380a16c6df29cad02a23c
Skeletus <Samol_88@hotmail.com>
2024-06-28 14:02:29 -0500

    added bey spin and updated changes on other beyblades

---
+++ BeybladeARMobileGame/Assets/Scripts/Beyblade.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Beyblade : MonoBehaviour
+{
+    [SerializeField] private float spinSpeed = 3600f;
+    [SerializeField] private bool doSpin = false;
+
+    [SerializeField] private GameObject playerGraphics;
+    private Rigidbody rigidBody;
+
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (doSpin)
+        {
+            playerGraphics.transform.Rotate(new Vector3(0, spinSpeed * Time.deltaTime, 0));
+        }
+    }
+}
