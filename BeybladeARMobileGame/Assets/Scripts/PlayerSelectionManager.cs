477c9d7d2f49a33596485ef47e6e13ac62a42b69
Skeletus <Samol_88@hotmail.com>
2024-07-01 11:05:39 -0500

    created player selection manager

---
+++ BeybladeARMobileGame/Assets/Scripts/PlayerSelectionManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerSelectionManager : MonoBehaviour
+{
+    [SerializeField] private Transform playerSwitcherTransform;
+
+    #region UNITY METHODS
+    #endregion
+
+    #region UI Callback Methods
+    public void NextPlayer()
+    {
+        StartCoroutine(Rotate(Vector3.up, playerSwitcherTransform, 90, 1.0f));
+    }
+
+    public void PreviousPlayer()
+    {
+        StartCoroutine(Rotate(Vector3.up, playerSwitcherTransform, -90, 1.0f));
+    }
+
+    #endregion
+
+    IEnumerator Rotate(Vector3 axis, Transform transformToRotate,
+        float angle, float duration = 1f)
+    {
+        Quaternion originalRotation = transformToRotate.rotation;
+        Quaternion finalRotation = transformToRotate.rotation * Quaternion.Euler(axis*angle);
+
+        float elapsedTime = 0.0f;
+        while(elapsedTime < duration)
+        {
+            transformToRotate.rotation = Quaternion.Slerp(originalRotation, finalRotation, elapsedTime/duration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transformToRotate.rotation = finalRotation;
+    }
+}
