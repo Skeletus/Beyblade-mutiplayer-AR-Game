a2566d51162041427aad51c7a123df880d0f8504
Skeletus <Samol_88@hotmail.com>
2024-07-01 11:44:55 -0500

    improving player selection code - refactor

--- BeybladeARMobileGame/Assets/Scripts/PlayerSelectionManager.cs
+++ BeybladeARMobileGame/Assets/Scripts/PlayerSelectionManager.cs
@@ -23,24 +23,21 @@ public class PlayerSelectionManager : MonoBehaviour
     {
         playerSelectionNumber = 0;
     }
-    #endregion
 
-    #region UI Callback Methods
-    public void NextPlayer()
+    private void DisablePrevAndNextButtons()
     {
-        playerSelectionNumber += 1;
-
-        if (playerSelectionNumber >= beybladeModels.Length)
-        {
-            playerSelectionNumber = 0;
-        }
-        Debug.Log(playerSelectionNumber);
-
         nextButton.enabled = false;
         prevButton.enabled = false;
+    }
 
-        StartCoroutine(Rotate(Vector3.up, playerSwitcherTransform, 90, 1.0f));
+    private void EnablePrevAndNextButtons()
+    {
+        nextButton.enabled = true;
+        prevButton.enabled = true;
+    }
 
+    private void ChangeBeybladeModelTypeText()
+    {
         if (playerSelectionNumber == 0 || playerSelectionNumber == 1)
         {
             // atack type
@@ -52,6 +49,25 @@ public class PlayerSelectionManager : MonoBehaviour
             playerModelTypeText.text = "Defense";
         }
     }
+    #endregion
+
+    #region UI Callback Methods
+    public void NextPlayer()
+    {
+        playerSelectionNumber += 1;
+
+        if (playerSelectionNumber >= beybladeModels.Length)
+        {
+            playerSelectionNumber = 0;
+        }
+        //Debug.Log(playerSelectionNumber);
+
+        DisablePrevAndNextButtons();
+
+        StartCoroutine(Rotate(Vector3.up, playerSwitcherTransform, 90, 1.0f));
+
+        ChangeBeybladeModelTypeText();
+    }
 
     public void PreviousPlayer()
     {
@@ -61,23 +77,13 @@ public class PlayerSelectionManager : MonoBehaviour
         {
             playerSelectionNumber = beybladeModels.Length - 1;
         }
-        Debug.Log(playerSelectionNumber);
+        //Debug.Log(playerSelectionNumber);
 
-        nextButton.enabled = false;
-        prevButton.enabled = false;
+        DisablePrevAndNextButtons();
 
         StartCoroutine(Rotate(Vector3.up, playerSwitcherTransform, -90, 1.0f));
 
-        if (playerSelectionNumber == 0 || playerSelectionNumber == 1)
-        {
-            // atack type
-            playerModelTypeText.text = "Attack";
-        }
-        else
-        {
-            // defense type
-            playerModelTypeText.text = "Defense";
-        }
+        ChangeBeybladeModelTypeText();
     }
 
     public void OnSelectButtonClicked()
@@ -105,7 +111,6 @@ public class PlayerSelectionManager : MonoBehaviour
 
         transformToRotate.rotation = finalRotation;
 
-        nextButton.enabled = true;
-        prevButton.enabled = true;
+        EnablePrevAndNextButtons();
     }
 }
