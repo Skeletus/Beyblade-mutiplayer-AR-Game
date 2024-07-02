925aa3cf3dddc1328bae52779e3307bf76e32c45
Skeletus <Samol_88@hotmail.com>
2024-07-02 12:46:47 -0500

    added messages and UI fields in beyblade game manager

--- BeybladeARMobileGame/Assets/Scripts/BeybladeGameManager.cs
+++ BeybladeARMobileGame/Assets/Scripts/BeybladeGameManager.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
+using TMPro;
 
 public class BeybladeGameManager : MonoBehaviourPunCallbacks
 {
+    [Header("UI")]
+    [SerializeField] private GameObject UI_InformPanelGameObject;
+    [SerializeField] private TextMeshProUGUI UI_InformText;
+
     #region UNITY Methods
+
+    private void Start()
+    {
+        UI_InformPanelGameObject.SetActive(true);
+        UI_InformText.text = "Search for games to battle";
+    }
+
     private void CreateAndJoinRoom()
     {
         string randomRoomName = "Room" + Random.Range(0,1000);
@@ -21,6 +33,7 @@ public class BeybladeGameManager : MonoBehaviourPunCallbacks
     #region UI Callback Methods
     public void JoinRandomRoom()
     {
+        UI_InformText.text = "Searching for available rooms...";
         PhotonNetwork.JoinRandomRoom();
     }
 
@@ -32,11 +45,18 @@ public class BeybladeGameManager : MonoBehaviourPunCallbacks
     public override void OnJoinRandomFailed(short returnCode, string message)
     {
         Debug.Log(message);
+        UI_InformText.text = message;
+
         CreateAndJoinRoom();
     }
 
     public override void OnJoinedRoom()
     {
+        if (PhotonNetwork.CurrentRoom.PlayerCount == 1)
+        {
+            UI_InformText.text = "Joined to: " + PhotonNetwork.CurrentRoom.Name
+                + " . Waiting for other player";
+        }
         Debug.Log(PhotonNetwork.NickName + " joined to " + PhotonNetwork.CurrentRoom.Name);
     }
 
@@ -44,6 +64,9 @@ public class BeybladeGameManager : MonoBehaviourPunCallbacks
     {
         Debug.Log(newPlayer.NickName + " joined to " + PhotonNetwork.CurrentRoom.Name
             + " player count: " + PhotonNetwork.CurrentRoom.PlayerCount);
+
+        UI_InformText.text = newPlayer.NickName + " joined to " + PhotonNetwork.CurrentRoom.Name
+            + " player count: " + PhotonNetwork.CurrentRoom.PlayerCount;
     }
 
     #endregion
