f9a1a57ebbf760483adada61ad526b3b1e2d13c0
Skeletus <Samol_88@hotmail.com>
2024-07-02 12:25:57 -0500

    added create random room when seraching for game

--- BeybladeARMobileGame/Assets/Scripts/BeybladeGameManager.cs
+++ BeybladeARMobileGame/Assets/Scripts/BeybladeGameManager.cs
@@ -2,14 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
 
 public class BeybladeGameManager : MonoBehaviourPunCallbacks
 {
+    #region UNITY Methods
+    private void CreateAndJoinRoom()
+    {
+        string randomRoomName = "Room" + Random.Range(0,1000);
+
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 2;
+
+        PhotonNetwork.CreateRoom(randomRoomName, roomOptions);
+    }
+    #endregion
+
     #region UI Callback Methods
     public void JoinRandomRoom()
     {
         PhotonNetwork.JoinRandomRoom();
     }
+
 
     #endregion
 
@@ -18,6 +32,13 @@ public class BeybladeGameManager : MonoBehaviourPunCallbacks
     public override void OnJoinRandomFailed(short returnCode, string message)
     {
         Debug.Log(message);
+        CreateAndJoinRoom();
+    }
+
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+        Debug.Log(PhotonNetwork.NickName + " joined to " + PhotonNetwork.CurrentRoom.Name);
     }
 
     #endregion
