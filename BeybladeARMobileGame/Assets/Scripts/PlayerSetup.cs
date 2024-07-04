2cd26dea98f65a81fa72f03f1ef616f040c4eb35
Skeletus <Samol_88@hotmail.com>
2024-07-03 23:02:17 -0500

    added show playername on top of beyblade

--- BeybladeARMobileGame/Assets/Scripts/PlayerSetup.cs
+++ BeybladeARMobileGame/Assets/Scripts/PlayerSetup.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using TMPro;
 
 public class PlayerSetup : MonoBehaviourPun
 {
+    [SerializeField] private TextMeshProUGUI playerNameText;
+
     private void Start()
     {
         if (photonView.IsMine)
@@ -18,6 +21,25 @@ public class PlayerSetup : MonoBehaviourPun
             // the player is remote player
             transform.GetComponent<MovementController>().enabled = false;
             transform.GetComponent<MovementController>().DisableJoystick();
+        }
+        SetPlayerName();
+    }
+
+    private void SetPlayerName()
+    {
+        if (playerNameText != null)
+        {
+            if (photonView.IsMine)
+            {
+                playerNameText.text = "YOU";
+                playerNameText.color = Color.green;
+            }
+            else
+            {
+                playerNameText.text = photonView.Owner.NickName;
+            }
+
+            
         }
     }
 }
