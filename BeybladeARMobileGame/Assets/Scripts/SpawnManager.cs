9f493d88c91903f276f96f79e0eda86265630d1d
Skeletus <Samol_88@hotmail.com>
2024-07-03 22:15:00 -0500

    added spawnPositions and instantiate beyblade prefab

--- BeybladeARMobileGame/Assets/Scripts/SpawnManager.cs
+++ BeybladeARMobileGame/Assets/Scripts/SpawnManager.cs
@@ -6,6 +6,7 @@ using Photon.Pun;
 public class SpawnManager : MonoBehaviourPunCallbacks
 {
     [SerializeField] private GameObject[] beybladePrefabs;
+    [SerializeField] private Transform[] spawnPositions;
 
     #region Photon Callback Methods
     public override void OnJoinedRoom()
@@ -16,7 +17,14 @@ public class SpawnManager : MonoBehaviourPunCallbacks
             if (PhotonNetwork.LocalPlayer.CustomProperties.TryGetValue(MultiplayerARBeybladeGame.PLAYER_SELECTION_NUMBER,
                 out beybladeSelectedIndex))
             {
-                Debug.Log("Selected beyblade -> " + (int)beybladeSelectedIndex);
+                //Debug.Log("Selected beyblade -> " + (int)beybladeSelectedIndex);
+
+                int randomSpawnPointIndex = Random.Range(0, spawnPositions.Length - 1);
+                Vector3 instantiatePosition = spawnPositions[randomSpawnPointIndex].position;
+
+                PhotonNetwork.Instantiate(beybladePrefabs[(int)beybladeSelectedIndex].name,
+                    instantiatePosition, Quaternion.identity);
+
             }
         }
 
