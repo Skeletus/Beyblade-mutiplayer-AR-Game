ba05d4b58039b985a2b8ab2ce0052508abab2679
Skeletus <Samol_88@hotmail.com>
2024-07-03 22:08:28 -0500

    added get index of selected beyblade

--- BeybladeARMobileGame/Assets/Scripts/SpawnManager.cs
+++ BeybladeARMobileGame/Assets/Scripts/SpawnManager.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-public class SpawnManager : MonoBehaviour
+public class SpawnManager : MonoBehaviourPunCallbacks
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject[] beybladePrefabs;
 
-    // Update is called once per frame
-    void Update()
+    #region Photon Callback Methods
+    public override void OnJoinedRoom()
     {
-        
+        if (PhotonNetwork.IsConnectedAndReady)
+        {
+            object beybladeSelectedIndex;
+            if (PhotonNetwork.LocalPlayer.CustomProperties.TryGetValue(MultiplayerARBeybladeGame.PLAYER_SELECTION_NUMBER,
+                out beybladeSelectedIndex))
+            {
+                Debug.Log("Selected beyblade -> " + (int)beybladeSelectedIndex);
+            }
+        }
+
     }
+    #endregion
 }
