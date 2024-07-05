95c0580720d005dcb3d37a377821697f104d3f2b
Skeletus <Samol_88@hotmail.com>
2024-07-04 20:31:13 -0500

    improved lag compensation in sycnrhonization script

--- BeybladeARMobileGame/Assets/Scripts/SynchronizationCustomScript.cs
+++ BeybladeARMobileGame/Assets/Scripts/SynchronizationCustomScript.cs
@@ -12,6 +12,14 @@ public class SynchronizationCustomScript : MonoBehaviour, IPunObservable
     private Vector3 networkedPosition;
     private Quaternion networkedRotation;
 
+    private bool synchronizeVelocity = true;
+    private bool synchronizeAngularVelocity = true;
+    private bool isTeleportEnabled = true;
+    private float teleportIfDistanceIsGreaterThan = 1.0f;
+
+    private float distance;
+    private float angle;
+
     private void Awake()
     {
         rigidBody = GetComponent<Rigidbody>();
@@ -26,9 +34,9 @@ public class SynchronizationCustomScript : MonoBehaviour, IPunObservable
         if(!photonView.IsMine)
         {
             rigidBody.position = Vector3.MoveTowards(rigidBody.position,
-            networkedPosition, Time.fixedDeltaTime);
+            networkedPosition, distance*(1.0f/PhotonNetwork.SerializationRate));
             rigidBody.rotation = Quaternion.RotateTowards(rigidBody.rotation,
-                networkedRotation, Time.fixedDeltaTime * 100);
+                networkedRotation, angle*(1.0f/PhotonNetwork.SerializationRate));
         }
 
         
@@ -42,12 +50,52 @@ public class SynchronizationCustomScript : MonoBehaviour, IPunObservable
             // should send position, velocity and data to the otther players
             stream.SendNext(rigidBody.position);
             stream.SendNext(rigidBody.rotation);
+
+            if (synchronizeVelocity)
+            {
+                stream.SendNext(rigidBody.velocity);
+            }
+            if(synchronizeAngularVelocity)
+            {
+                stream.SendNext(rigidBody.angularVelocity);
+            }
         }
         else
         {
             // called on my player gameobject that exits in remote player's game
             networkedPosition = (Vector3)stream.ReceiveNext();
             networkedRotation = (Quaternion)stream.ReceiveNext();
+
+            if(isTeleportEnabled)
+            {
+                if(Vector3.Distance(rigidBody.position, networkedPosition) > teleportIfDistanceIsGreaterThan)
+                {
+                    rigidBody.position = networkedPosition;
+                }
+            }
+
+            if (synchronizeVelocity || synchronizeAngularVelocity)
+            {
+                float lag = Mathf.Abs((float)(PhotonNetwork.Time - info.SentServerTime));
+
+                if(synchronizeVelocity)
+                {
+                    rigidBody.velocity = (Vector3)stream.ReceiveNext();
+
+                    networkedPosition += rigidBody.velocity * lag;
+
+                    distance = Vector3.Distance(rigidBody.position, networkedPosition);
+                }
+
+                if(synchronizeAngularVelocity)
+                {
+                    rigidBody.angularVelocity = (Vector3)stream.ReceiveNext();
+
+                    networkedRotation = Quaternion.Euler(rigidBody.angularVelocity*lag) * networkedRotation;
+
+                    angle = Quaternion.Angle(rigidBody.rotation, networkedRotation);
+                }
+            }
         }
     }
 }
