d4bd9717d4a8312741583150598a5ef0a01e8278
Skeletus <Samol_88@hotmail.com>
2024-07-09 18:02:33 -0500

    completd player instantation with reise events

--- BeybladeARMobileGame/Assets/Scripts/SpawnManager.cs
+++ BeybladeARMobileGame/Assets/Scripts/SpawnManager.cs
@@ -11,6 +11,16 @@ public class SpawnManager : MonoBehaviourPunCallbacks
     [SerializeField] private Transform[] spawnPositions;
     [SerializeField] private GameObject battleArenaGameObject;
 
+    private void Start()
+    {
+        PhotonNetwork.NetworkingClient.EventReceived += OnEvent;
+    }
+
+    private void OnDestroy()
+    {
+        PhotonNetwork.NetworkingClient.EventReceived -= OnEvent;
+    }
+
     public enum RaiseEventCodes
     {
         PlayerSpawnEventCode = 0
@@ -67,27 +77,28 @@ public class SpawnManager : MonoBehaviourPunCallbacks
     }
 
     #region Photon Callback Methods
+    public void OnEvent(EventData photonEvent)
+    {
+        if(photonEvent.Code == (byte)RaiseEventCodes.PlayerSpawnEventCode)
+        {
+            object[] data = (object[])photonEvent.CustomData;
+            Vector3 receivedPosition = (Vector3)data[0];
+            Quaternion receivedRotation = (Quaternion)data[1];
+            int receivedBeybladeSelectedIndexData = (int)data[3];
+
+            GameObject player = Instantiate(beybladePrefabs[receivedBeybladeSelectedIndexData],
+                receivedPosition + battleArenaGameObject.transform.position, receivedRotation);
+
+            PhotonView photonView = player.GetComponent<PhotonView>();
+            photonView.ViewID = (int)data[2];
+        }
+    }
+
     public override void OnJoinedRoom()
     {
         if (PhotonNetwork.IsConnectedAndReady)
         {
             SpawnPlayer();
-
-            /*
-            object beybladeSelectedIndex;
-            if (PhotonNetwork.LocalPlayer.CustomProperties.TryGetValue(MultiplayerARBeybladeGame.PLAYER_SELECTION_NUMBER,
-                out beybladeSelectedIndex))
-            {
-                //Debug.Log("Selected beyblade -> " + (int)beybladeSelectedIndex);
-
-                int randomSpawnPointIndex = Random.Range(0, spawnPositions.Length - 1);
-                Vector3 instantiatePosition = spawnPositions[randomSpawnPointIndex].position;
-
-                PhotonNetwork.Instantiate(beybladePrefabs[(int)beybladeSelectedIndex].name,
-                    instantiatePosition, Quaternion.identity);
-
-            }
-            */
         }
 
     }
